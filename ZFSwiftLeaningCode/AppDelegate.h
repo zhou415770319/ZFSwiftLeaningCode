6e979ae45390a747c07ffe4d4eaf2f57ecb35e46
zhoufei <415770319@qq.com>
2016-11-21 11:22:21 +0800

    add readme

---
+++ ZFSwiftLeaningCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZFSwiftLeaningCode
+//
+//  Created by 周飞 on 2016/11/21.
+//  Copyright © 2016年 周飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
